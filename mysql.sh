7a412da780b1ea0ae32adcfc7650ac42bbcca2bc
G Manjunth <manjudba1222@gmail.com>
2025-01-03 09:29:51 +0530

    expense

---
+++ mysql.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+
+PATH1="/var/log/shell-script"
+FILE1="log.txt"
+FULLPATH="$PATH1/$FILE1"
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then 
+echo -e "$G $2..succussful" 
+else 
+echo -e "$R $2 Failed" 
+exist 1
+fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+
+echo " User not having sufficent permissions"
+exist 1
+else
+
+ dnf list installed |grep mysql &>$FULLPATH
+
+ if [ $? -eq 0 ]
+then 
+echo -e "$G Already Installed MYSQL"
+else
+dnf install mysql-server -y &>$FULLPATH
+
+VALIDATE $? "mysql" 
+
+systemctl enable mysqld &>$FULLPATH
+VALIDATE $? "enable mysql" 
+
+systemctl start mysqld &>$FULLPATH
+VALIDATE $? "start mysql" 
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>$FULLPATH
+VALIDATE $? "Set user password" 
+fi
+fi
+
+
